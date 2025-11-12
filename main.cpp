47d9153cf1b3f6ca02d299a7eea36cec7ae24efb
SuhaMohamedElguindy <suha.elguindy@student.giu-uni.de>
2025-11-12 15:47:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,15 @@ using namespace std;
 
 // ============== MAIN FUNCTION ==============
 int main() {
-    
+Enclosure* A=new Enclosure(&animal,10,0);
+Animal mammal=new Mammal("Lion",5,1);
+Animal bird=new Bird("Parrot",2,0);
+Animal reptile=new Reptile("Snake",3,1,1);
+Visitor v=new Visitor("Sarah Ali",3);
+cout<<"Enclosure 1 Animals:"<<endl;
+cout<<Animals->displayAnimals()<<endl;
+cout<<"VisitorInfo:"<<endl;
+cout<<v->displayInfo()<<endl;
     
     return 0;
 }
